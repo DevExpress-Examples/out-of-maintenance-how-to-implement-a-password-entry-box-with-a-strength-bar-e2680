37b81bf44b558cbe52432002a81143d563f10801
DevExpressExampleBot <fakeEmail>
2018-12-03 14:31:48 +0300

    Source auto update [skip ci]

--- VB/WebSite/Complete.aspx.vb
+++ VB/WebSite/Complete.aspx.vb
@@ -1,18 +1,9 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Data
-Imports System.Configuration
-Imports System.Collections
-Imports System.Web
-Imports System.Web.Security
-Imports System.Web.UI
-Imports System.Web.UI.WebControls
-Imports System.Web.UI.WebControls.WebParts
-Imports System.Web.UI.HtmlControls
+﻿Imports System
 
 Partial Public Class Complete
-	Inherits System.Web.UI.Page
-	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+    Inherits System.Web.UI.Page
 
-	End Sub
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
 End Class
--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -1,25 +1,20 @@
-﻿Imports Microsoft.VisualBasic
+﻿Imports DevExpress.Web
 Imports System
-Imports System.Collections.Generic
-Imports System.Web
-Imports System.Web.UI
-Imports System.Web.UI.WebControls
-Imports DevExpress.Web
 
 Partial Public Class _Default
-	Inherits System.Web.UI.Page
+    Inherits System.Web.UI.Page
 
-	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-	End Sub
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+    End Sub
 
-	Protected Sub tbConfirmedPassword_Validation(ByVal sender As Object, ByVal e As DevExpress.Web.ValidationEventArgs)
-		If e.IsValid AndAlso tbPassword.Text <> tbConfirmedPassword.Text Then
-			e.ErrorText = "Passwords do not match"
-			e.IsValid = False
-		End If
+    Protected Sub tbConfirmedPassword_Validation(ByVal sender As Object, ByVal e As ValidationEventArgs)
+        If e.IsValid AndAlso tbPassword.Text <> tbConfirmedPassword.Text Then
+            e.ErrorText = "Passwords do not match"
+            e.IsValid = False
+        End If
 
-		If e.IsValid Then
-			Response.Redirect("Complete.aspx")
-		End If
-	End Sub
+        If e.IsValid Then
+            Response.Redirect("Complete.aspx")
+        End If
+    End Sub
 End Class
